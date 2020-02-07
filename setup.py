1787329c4fa968e28bfab99fb724dba03ac199e1
Henry Jackson-Flux <hejackso@microsoft.com>
2020-02-07 17:33:58 +0000

    chore(*): version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name="tf2_gnn",
-    version="0.9.10",
+    version="0.9.11",
     description="TensorFlow 2.0 implementation of Graph Neural Networks.",
     python_requires=">=3.6",
     install_requires=["numpy", "docopt", "dpu-utils>=0.2.7", "h5py"],
