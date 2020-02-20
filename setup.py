26dc36646e6d91b88a155eb970874349686b7b92
megstanley <t-mestan@microsoft.com>
2020-02-20 18:50:19 +0000

    update version

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ import setuptools
 
 setuptools.setup(
     name="tf2_gnn",
-    version="0.9.14",
+    version="0.9.15",
     description="TensorFlow 2.0 implementation of Graph Neural Networks.",
     python_requires=">=3.6",
     install_requires=["numpy", "docopt", "dpu-utils>=0.2.7", "h5py"],
-    packages=setuptools.find_packages(where='.'),
-    package_dir={'':'.'},
-    package_data={'': ['default_hypers/*.json']},
+    packages=setuptools.find_packages(where="."),
+    package_dir={"": "."},
+    package_data={"": ["default_hypers/*.json"]},
     entry_points={
-        'console_scripts': [
-            'tf2_gnn_train = tf2_gnn.cli.train:run',
-            'tf2_gnn_test = tf2_gnn.cli.test:run',
-        ],
+        "console_scripts": [
+            "tf2_gnn_train = tf2_gnn.cli.train:run",
+            "tf2_gnn_test = tf2_gnn.cli.test:run",
+        ]
     },
 )
