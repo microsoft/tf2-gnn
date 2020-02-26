41ce153f77a607a174089ba77a8a2e04cb075eb7
Marc Brockschmidt <marc@marcbrockschmidt.de>
2020-02-26 18:44:39 +0000

    chore(setup.py): Fill out setup.py with more info

--- setup.py
+++ setup.py
@@ -2,16 +2,29 @@
 """Make the module pip installable."""
 
 import setuptools
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setuptools.setup(
     name="tf2_gnn",
     version="1.0.0",
+    license="MIT",
     author="Marc Brockschmidt",
     author_email="mabrocks@microsoft.com",
     description="TensorFlow 2.0 implementation of Graph Neural Networks.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/microsoft/tf2-gnn/",
     python_requires=">=3.6",
-    install_requires=["numpy", "docopt", "dpu-utils>=0.2.7", "h5py"],
+    install_requires=[
+        "numpy",
+        "tensorflow>=2.0.0",
+        "dpu-utils>=0.2.7",
+        "h5py",
+    ],
     packages=setuptools.find_packages(where="."),
     package_dir={"": "."},
     package_data={"": ["default_hypers/*.json"]},
