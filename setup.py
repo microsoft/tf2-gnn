c6957507f4ca09686d68776d8db5faeb7f881fa2
Marc Brockschmidt <marc@marcbrockschmidt.de>
2020-02-26 17:13:55 +0000

    chore(setup.py): Add info to setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,11 @@ import setuptools
 
 setuptools.setup(
     name="tf2_gnn",
-    version="0.9.21",
+    version="1.0.0",
+    author="Marc Brockschmidt",
+    author_email="mabrocks@microsoft.com",
     description="TensorFlow 2.0 implementation of Graph Neural Networks.",
+    url="https://github.com/microsoft/tf2-gnn/",
     python_requires=">=3.6",
     install_requires=["numpy", "docopt", "dpu-utils>=0.2.7", "h5py"],
     packages=setuptools.find_packages(where="."),
@@ -18,4 +21,9 @@ setuptools.setup(
             "tf2_gnn_test = tf2_gnn.cli.test:run",
         ]
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
