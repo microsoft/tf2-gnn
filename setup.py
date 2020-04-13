4b415a57c8d897c01e5c06399b08ab16cb75a809
Marc Brockschmidt <marc@marcbrockschmidt.de>
2020-04-13 12:04:37 +0000

    chore(setup): Bump micro version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setuptools.setup(
     name="tf2_gnn",
-    version="2.1.0",
+    version="2.1.1",
     license="MIT",
     author="Marc Brockschmidt",
     author_email="mabrocks@microsoft.com",
